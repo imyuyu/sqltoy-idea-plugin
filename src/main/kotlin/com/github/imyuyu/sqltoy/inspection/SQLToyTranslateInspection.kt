19d7de46c89661f20e447fa2a56ebe96224e0917
imyuyu <2075904@qq.com>
2024-07-06 20:07:49 +0800

    :sparkles: feat: `*-translate.xml` inspection

--- src/main/kotlin/com/github/imyuyu/sqltoy/inspection/SQLToyTranslateInspection.kt
+++ src/main/kotlin/com/github/imyuyu/sqltoy/inspection/SQLToyTranslateInspection.kt
@@ -1,6 +1,6 @@
 package com.github.imyuyu.sqltoy.inspection
 
-import com.github.imyuyu.sqltoy.dom.model.sql.SQLToy
+import com.github.imyuyu.sqltoy.dom.model.translate.SQLToyTranslate
 import com.intellij.util.xml.DomElement
 import com.intellij.util.xml.highlighting.BasicDomElementsInspection
 import com.intellij.util.xml.highlighting.DomElementAnnotationHolder
@@ -10,8 +10,8 @@ import com.intellij.util.xml.highlighting.DomHighlightingHelper
  * XML 检查
  *
  */
-class SQLToyXmlInspection : BasicDomElementsInspection<SQLToy?>(
-    SQLToy::class.java
+class SQLToyTranslateInspection : BasicDomElementsInspection<SQLToyTranslate?>(
+    SQLToyTranslate::class.java
 ) {
     override fun checkDomElement(
         element: DomElement,
@@ -21,7 +21,7 @@ class SQLToyXmlInspection : BasicDomElementsInspection<SQLToy?>(
         super.checkDomElement(element, holder, helper)
     }
 
-    override fun getStaticDescription(): String? {
-        return "Static SQLToyXmlInspection"
+    override fun getStaticDescription(): String {
+        return "Static SQLToy Translate Inspection"
     }
 }
